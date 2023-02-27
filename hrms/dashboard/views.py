38461f6034b2d406ea9abfabec56b8608ef9a031
hchamikadilshan <hchamikadishan@gmail.com>
2023-02-27 20:56:04 +0530

    dashboard added total_active_employee_count

--- hrms/dashboard/views.py
+++ hrms/dashboard/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from employee.models import Employee
 
 # Create your views here.
 
-class DashboardMainView(View):
+class DashboardMainView(LoginRequiredMixin,View):
     def get(self,request):
-        if not request.user.is_authenticated:
-            return redirect("login_user_url")
-        else:
-            return render(request, 'dashboard.html')
+        login_url = '/accounts/login'
+        employee_record = Employee.objects.filter(active_status=True)
+        employee_record_list = list(employee_record)
+        active_employees = len(employee_record_list)
+        print(active_employees)
+        return render (request,"dashboard.html",context={'no_of_employees':active_employees})
