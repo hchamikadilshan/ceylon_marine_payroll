8e2554050423dfdbc0ec49cabf8ed2179eb3d05a
hchamikadilshan <hchamikadishan@gmail.com>
2023-04-08 19:24:20 +0530

    signature report update

--- hrms/reports/views.py
+++ hrms/reports/views.py
@@ -58,7 +58,7 @@ Allowance""",'EPF',"""Total
 Deductions""", 'Net Salary','Signature']
         table_data.append(table_heading)
         for emp in response_employees:
-            table_row = [emp[0], emp[1], emp[2],emp[3], emp[4], emp[5],emp[0], emp[1], emp[2],emp[3], emp[4], ""]
+            table_row = [emp[0], emp[1], emp[2],emp[3], emp[4], emp[5],emp[6], emp[7], emp[8],emp[9], emp[10], ""]
             table_data.append(table_row)
         elements = []
         attendance_table = Table(table_data,colWidths=[0.6*inch,2.5*inch,0.8*inch,0.8*inch,0.8*inch,0.8*inch,0.8*inch,0.6*inch,0.7*inch,0.8*inch,2.0*inch],rowHeights=[0.3*inch for i in range(len(response_employees)+3)])
