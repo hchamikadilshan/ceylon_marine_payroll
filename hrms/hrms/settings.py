0387a30b4c34011a8a24fcbd013c1a8ddb6aafe4
hchamikadilshan <hchamikadishan@gmail.com>
2023-05-09 16:36:16 +0530

    changed database local to online

--- hrms/hrms/settings.py
+++ hrms/hrms/settings.py
@@ -85,33 +85,33 @@ WSGI_APPLICATION = 'hrms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-# DATABASES = {
-# 	'default': {
-# 		'ENGINE': 'django.db.backends.mysql',
-# 		'NAME': 'heroku_224badc376eea21',
-# 		'USER': 'bff5e7e4628e19',
-# 		'PASSWORD': '01625560',
-# 		'HOST': 'us-cdbr-east-06.cleardb.net',
-# 		'PORT':'3306',
-# 	}
-# }
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.mysql',
-		'NAME': 'payroll_test',
-		'USER': 'root',
-		'PASSWORD': '[Python2001mysql]',
-		'HOST': '127.0.0.1',
+		'NAME': 'heroku_224badc376eea21',
+		'USER': 'bff5e7e4628e19',
+		'PASSWORD': '01625560',
+		'HOST': 'us-cdbr-east-06.cleardb.net',
 		'PORT':'3306',
 	}
 }
+
+# DATABASES = {
+# 	'default': {
+# 		'ENGINE': 'django.db.backends.mysql',
+# 		'NAME': 'payroll_test',
+# 		'USER': 'root',
+# 		'PASSWORD': '[Python2001mysql]',
+# 		'HOST': '127.0.0.1',
+# 		'PORT':'3306',
+# 	}
+# }
 
 
 
