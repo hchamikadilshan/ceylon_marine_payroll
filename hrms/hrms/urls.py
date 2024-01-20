bf1075ef30442db3adb43a2cd9737f51d8fe64ac
hchamikadilshan <hchamikadishan@gmail.com>
2024-01-20 20:01:07 +0530

    added redirect to dashoard url

--- hrms/hrms/urls.py
+++ hrms/hrms/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: redirect('dashboard/')),
     path('accounts/',include('accounts.urls')),
     path('dashboard/',include('dashboard.urls')),
     path('employee_management/',include('employee.urls')),
